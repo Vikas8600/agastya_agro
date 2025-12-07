4e4aced9298ce215dc47dbb2353e9144d7fb3e14
Hyb-Kaushik <kaushik@mail.hybrowlabs.com>
2025-12-07 22:28:29 +0530

    internal stock transfer issue resolved

--- agastya_agro/public/py/purchase_invoice.py
+++ agastya_agro/public/py/purchase_invoice.py
@@ -1,4 +1,5 @@
 import frappe
+from frappe.utils import flt
 
 def validate(doc, method):
     customer_name = frappe.db.get_value("Customer", {
@@ -17,26 +18,71 @@ def validate(doc, method):
         if not dn:
             return
 
-        # Get DN items that were used to create this PR
-        # by matching dn_detail from PR items
+        # Get PR items with their DN detail mapping
         pr_items = frappe.get_all("Purchase Receipt Item",
             filters={"parent": pr_name},
-            fields=["custom_delivery_note_detail"]
+            fields=["name", "item_code", "qty", "custom_delivery_note_detail"]
         )
-        # frappe.throw(str(pr_items))
+
         dn_detail_list = [item.custom_delivery_note_detail for item in pr_items if item.custom_delivery_note_detail]
 
         if not dn_detail_list:
             return
-        # Find Sales Invoice Items that reference the same DN items
-        # SI Item has delivery_note and dn_detail fields
-        si_item = frappe.get_value("Sales Invoice Item", {
-            "delivery_note": dn,
-            "dn_detail": ["in", dn_detail_list]
-        }, "parent")
 
-        if si_item:
-            doc.custom_sales_invoice = si_item
+        # Find ALL Sales Invoice Items that reference the same DN items
+        si_items = frappe.get_all("Sales Invoice Item",
+            filters={
+                "delivery_note": dn,
+                "dn_detail": ["in", dn_detail_list],
+                "docstatus": 1
+            },
+            fields=["name", "parent", "qty", "item_code", "dn_detail"],
+            order_by="creation asc"
+        )
+
+        if not si_items:
+            return
+
+        # Build a map of dn_detail -> list of SI items (for items with same dn_detail)
+        dn_detail_to_si_items = {}
+        for si_item in si_items:
+            if si_item.dn_detail not in dn_detail_to_si_items:
+                dn_detail_to_si_items[si_item.dn_detail] = []
+            dn_detail_to_si_items[si_item.dn_detail].append(si_item)
+
+        # For each PI item, find the correct SI item and set custom_sales_invoice_detail
+        selected_si = None
+        for pi_item in doc.items:
+            # Find the corresponding PR item to get the dn_detail
+            pr_item = next((p for p in pr_items if p.item_code == pi_item.item_code), None)
+            if not pr_item or not pr_item.custom_delivery_note_detail:
+                continue
+
+            dn_detail = pr_item.custom_delivery_note_detail
+            matching_si_items = dn_detail_to_si_items.get(dn_detail, [])
+
+            # Find SI item with remaining qty to bill
+            for si_item in matching_si_items:
+                # Get already billed qty for this SI item from other submitted Purchase Invoices
+                billed_qty = flt(frappe.db.get_value(
+                    "Purchase Invoice Item",
+                    {
+                        "custom_sales_invoice_detail": si_item.name,
+                        "docstatus": 1
+                    },
+                    "sum(qty)"
+                ) or 0)
+
+                remaining_qty = flt(si_item.qty) - billed_qty
+
+                # If this SI item has remaining qty, use it
+                if remaining_qty >= flt(pi_item.qty):
+                    pi_item.custom_sales_invoice_detail = si_item.name
+                    selected_si = si_item.parent
+                    break
+
+        if selected_si:
+            doc.custom_sales_invoice = selected_si
             doc.write_off_amount = doc.rounded_total
             doc.write_off_account = "Stock Transfer Write Off - AAL"
             doc.write_off_cost_center = doc.cost_center
